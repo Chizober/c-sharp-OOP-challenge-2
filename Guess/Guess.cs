985f6923ea723ff4d6fee21e3302dd9b41083996
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2022-11-14 18:10:31 +0100

    Add files via upload

---
+++ Guess/Guess.cs
@@ -0,0 +1,53 @@
+﻿using System;
+ namespace Guess
+ {
+  class GuessANumber
+{
+    int guess = 0;
+    int guessCount = 0;
+    int secretNumber = new Random().Next(1,10);
+   public void Options()
+   {
+   while(guess != secretNumber)
+   {
+    try{
+
+   Console.WriteLine("Enter a number between 1 to 10");
+    guess= Convert.ToInt32(Console.ReadLine());
+  if(guess < secretNumber)
+  {
+   Console.WriteLine($"{guess} is too small");
+   guessCount++;
+   continue;
+   }
+  else if(guess > secretNumber)
+   {
+       Console.WriteLine($"{guess} is too large");
+       guessCount++;
+       continue;
+   }
+    }
+    catch
+    {
+      Console.WriteLine("Not an integer, try again");
+      guessCount++;
+      continue;
+    }
+    guessCount++;
+       Console.WriteLine("guessed correctly!,secret number was {0} " ,secretNumber);
+       Console.WriteLine("It took you {0} {1}.\n", guessCount, guessCount==1?"try": "tries");
+       break;
+   }
+   }
+}
+
+  class Program{
+  static void Main(string[] args)
+  {
+   GuessANumber guesses = new GuessANumber();
+  guesses.Options();
+}
+ }
+ }
+ 
+
